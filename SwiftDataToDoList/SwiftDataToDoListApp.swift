8019bb112ac2549cc3d3716265cc6515967b06d7
MaxGarcia98 <75860997+MaxGarcia98@users.noreply.github.com>
2023-11-02 16:16:38 -0400

    Initial Commit

---
+++ SwiftDataToDoList/SwiftDataToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataToDoListApp.swift
+//  SwiftDataToDoList
+//
+//  Created by Maximino Garcia II on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
